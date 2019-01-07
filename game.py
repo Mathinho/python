bd01bf55bf16b0327acd3219734ee7d79d1a79aa
Mathinho <mwenk@albg.local>
2019-01-07 12:21:46 +0100

    added image animation

--- game.py
+++ game.py
@@ -5,14 +5,43 @@ GRAY = (200, 200, 200)
 RED = (255, 0, 0)
 
 pygame.init()
+
+FPS = 30 # frames per second setting
+fpsClock = pygame.time.Clock()
+
 DISPLAYSURF = pygame.display.set_mode((600, 400))
 pygame.display.set_caption('Python Game')
-DISPLAYSURF.fill(GRAY)
-pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
+
+image = pygame.image.load('test.png')
+imageX = 10
+imageY = 10
+direction = 'right'
 
 while True:
+    DISPLAYSURF.fill(GRAY)
+
+    if direction == 'right':
+        imageX += 5
+    if imageX == 280:
+        direction = 'down'
+    elif direction == 'down':
+        imageY += 5
+    if imageY == 220:
+        direction = 'left'
+    elif direction == 'left':
+        imageX -= 5
+    if imageX == 10:
+        direction = 'up'
+    elif direction == 'up':
+        imageY -= 5
+    if imageY == 10:
+        direction = 'right'
+
+    DISPLAYSURF.blit(image, (imageX, imageY))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
     pygame.display.update()
