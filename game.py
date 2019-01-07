a84328cff6509d6dd48399a6135f19dd94ec0538
Mathinho <mwenk@albg.local>
2019-01-07 12:09:54 +0100

    changed color

--- game.py
+++ game.py
@@ -1,9 +1,15 @@
 import pygame, sys
 from pygame.locals import *
 
+GRAY = (200, 200, 200)
+RED = (255, 0, 0)
+
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World')
+DISPLAYSURF = pygame.display.set_mode((600, 400))
+pygame.display.set_caption('Python Game')
+DISPLAYSURF.fill(GRAY)
+pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
