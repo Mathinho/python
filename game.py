e5d97b1f9f35facb80e79cc38d7823a672b74173
Mathinho <mwenk@albg.local>
2019-01-11 16:49:54 +0100

    added rectangle frame

--- game.py
+++ game.py
@@ -5,7 +5,7 @@
 import pygame, sys
 from pygame.locals import *
 
-GRAY = (200, 200, 200)
+GREY = (200, 200, 200)
 RED = (200, 0, 0)
 
 pygame.init()
@@ -14,9 +14,9 @@ FPS = 60 # frames per second setting
 fpsClock = pygame.time.Clock()
 
 #define window size
-DISPLAYSURF = pygame.display.set_mode((1200, 1000))
+screen = pygame.display.set_mode((1200, 1000))
 pygame.NOFRAME
-pygame.display.set_caption('Martas Quest')
+pygame.display.set_caption('Super Marta')
 
 #load various images
 imageLEFT = pygame.image.load('marioLEFT.png')
@@ -33,32 +33,32 @@ theme = pygame.mixer.music.load('SuperMarioBros.ogg')
 pygame.mixer.music.play()
 
 while True:
-    DISPLAYSURF.fill(GRAY)
+    screen.fill(GREY)
+
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 400, 400), 2)
 
     #get keyboard to move character
     key = pygame.key.get_pressed()
-    if key [pygame.K_LEFT]:
-        DISPLAYSURF.blit(imageLEFT, (imageX, imageY))
+    if key [pygame.K_LEFT] or key [pygame.K_a]:
+        screen.blit(imageLEFT, (imageX, imageY))
         imageX -= 1
         imageLAST = imageLEFT
-    elif key [pygame.K_RIGHT]:
-        DISPLAYSURF.blit(imageRIGHT, (imageX, imageY))
+    elif key [pygame.K_RIGHT] or key [pygame.K_d]:
+        screen.blit(imageRIGHT, (imageX, imageY))
         imageX += 1
         imageLAST = imageRIGHT
-    elif key [pygame.K_DOWN]:
-        DISPLAYSURF.blit(imageRIGHT, (imageX, imageY))
+    elif key [pygame.K_DOWN] or key [pygame.K_s]:
+        screen.blit(imageLAST, (imageX, imageY))
         imageY += 1
-        imageLAST = imageRIGHT
-    elif key [pygame.K_UP]:
-        DISPLAYSURF.blit(imageRIGHT, (imageX, imageY))
-        imageY -= 1
-        imageLAST = imageRIGHT
+    elif key [pygame.K_UP] or key [pygame.K_w]:
+        screen.blit(imageLAST, (imageX, imageY))
+        imageY -= 1        
 
     if imageX >= imageY >= 100:
-        DISPLAYSURF.fill(RED)
+        screen.fill(RED)
 
     #show character
-    DISPLAYSURF.blit(imageLAST, (imageX, imageY))
+    screen.blit(imageLAST, (imageX, imageY))
 
     for event in pygame.event.get():
         if event.type == QUIT:
