7a29ee008d4895259f1ac0d37c1394d37d72e22b
Mathinho <mwenk@albg.local>
2019-01-24 16:30:13 +0100

    Pin Belegung geändert

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,8 +1,8 @@
 from machine import pin
 import time
 
-p0 = Pin(0, Pin.OUT)    # create output pin on GPIO0
+p0 = Pin(2, Pin.OUT)    # create output pin on GPIO0
 p0.on()                 # set pin to "on" (high) level
-time.sleep(100)
+time.sleep(1)
 p0.off()
-time.sleep(100)
+time.sleep(1)
