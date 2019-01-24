624bfae40a7201d8cafc07949fe3b07057286542
Mathinho <mwenk@albg.local>
2019-01-24 16:48:07 +0100

    wifi added

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -3,6 +3,19 @@ import time
 
 p0 = Pin(0, Pin.OUT)    # create output pin on GPIO0
 
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('albg_wlan', 'boardalpha@0.9.168.192#14albg')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+do_connect()
+
 while True:
     p0.on()                 # set pin to "on" (high) level
     time.sleep(1)
