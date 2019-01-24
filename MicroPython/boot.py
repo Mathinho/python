1e0d02ab339923b68aeb9f6565f365c535281863
Mathinho <mwenk@albg.local>
2019-01-24 17:15:46 +0100

    webserver added

---
+++ MicroPython/boot.py
@@ -0,0 +1,31 @@
+#pylint:disable=E0401
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'albg_wlan'
+password = 'boardalpha@0.9.168.192#14albg'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+led = Pin(0, Pin.OUT)
