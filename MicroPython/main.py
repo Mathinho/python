a23dd8a187a7900231b4be5acb8df447397597d7
Mathinho <mathinho@mathinhos-imac.speedport_w_724v_typ_a_05011603_06_001>
2019-02-02 20:27:18 +0100

    cleaned up

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,21 +1,15 @@
 import ili9341, time, bitmapfont, math
 from machine import Pin, SPI
 from rgb import color565
-#spi = SPI(mosi=Pin(13), sck=Pin(14), miso=Pin(12))
-#hspi = SPI(1, 80000000, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
-vspi = SPI(2, baudrate=80000000, polarity=0, phase=0, bits=8, firstbit=0, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
-display = ili9341.ILI9341(vspi, cs=Pin(15), dc=Pin(4), rst=Pin(16))
+hspi = SPI(1, 80000000, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
+display = ili9341.ILI9341(hspi, cs=Pin(15), dc=Pin(4), rst=Pin(16))
 display.fill(color565(255, 0, 0))
-#display.fill(ili9341.color565(0xff, 0x11, 0x22))
-#display.pixel(120, 160, 0)
-time.sleep(1)
+time.sleep(0.1)
 display.fill(0)
-time.sleep(1)
+time.sleep(0.1)
 display.pixel(200, 200, color565(255, 255, 255))
-time.sleep(1)
+time.sleep(0.1)
 display.fill_rectangle(0, 0, 120, 170, color565(0, 0, 255))
-#display.text('test', 100, 100, 100)
-#text(display, "test", 100, 100)
 display.hline(100, 20, 100, color565(0, 255, 0))
 display.vline(100, 20, 100, color565(0, 255, 0))
 
